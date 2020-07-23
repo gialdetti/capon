29b4bf92ceef091a8f49ffc5a6fcb97ac971d30b
eyalgal <eyal@gng.ai>
2020-07-23 11:39:48 +0300

    MAINT: version 0.0.3 - capon template

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["numpy>=1.17.3", "pandas>=1.0.3", "requests>=2.22.0", "plotly>=4
 
 setup(
     name="capon",
-    version="0.0.2",
+    version="0.0.3",
     author="Eyal Gal",
     author_email="eyalgl@gmail.com",
     description="Capital Market in Python",
