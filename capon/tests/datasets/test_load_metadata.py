c3d65b68de7c55a7a51a6126cd7b549b055099d0
Eyal Gal <eyalgl@gmail.com>
2022-06-30 14:50:30 +0300

    DATA: update metadata

--- capon/tests/datasets/test_load_metadata.py
+++ capon/tests/datasets/test_load_metadata.py
@@ -1,9 +1,12 @@
-import logging; logging.basicConfig(format='[%(asctime)s] %(message)s', level=logging.INFO)
+import logging
+
+logging.basicConfig(format="[%(asctime)s] %(message)s", level=logging.INFO)
 
 from capon.datasets import load_metadata
 
 
 def test():
-    load_metadata()
-
-
+    metadata = load_metadata()
+    metadata["sector"].value_counts()
+    metadata["industry"].value_counts()
+    metadata["updated_at"].value_counts().sort_index()
