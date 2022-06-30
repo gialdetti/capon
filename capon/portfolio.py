a9e4c51aacc64bf37c17b8b9c79d010cac675e50
Eyal Gal <eyalgl@gmail.com>
2022-06-30 11:09:08 +0300

    FIX: extend portfolio range to 10y

--- capon/portfolio.py
+++ capon/portfolio.py
@@ -3,7 +3,7 @@ import pandas as pd
 from capon.backends import yahoo as yf
 
 
-default_indexes = ['^GSPC', '^DJI', '^IXIC', '^RUT']
+default_indexes = ["^GSPC", "^DJI", "^IXIC", "^RUT"]
 
 
 class Lot(typing.NamedTuple):
@@ -19,6 +19,7 @@ class Lot(typing.NamedTuple):
     price: float
         Cost per share
     """
+
     timestamp: str
     symbol: str
     quantity: int
@@ -26,45 +27,69 @@ class Lot(typing.NamedTuple):
     cost: float = None
 
 
-class Portfolio():
+class Portfolio:
     def __init__(self, lots):
-        lots = pd.DataFrame(lots) \
-            .assign(timestamp=lambda df: pd.to_datetime(df['timestamp']))
-        lots['cost'] = lots['cost'].where(lots['cost'].notna(), lots['price']*lots['quantity']).astype({'cost': float})
+        lots = pd.DataFrame(lots).assign(
+            timestamp=lambda df: pd.to_datetime(df["timestamp"])
+        )
+        lots["cost"] = (
+            lots["cost"]
+            .where(lots["cost"].notna(), lots["price"] * lots["quantity"])
+            .astype({"cost": float})
+        )
         self.lots = lots
 
     def status(self, anchor=None):
         performance = self.performance(anchor=anchor)
-        latest = performance.groupby(['timestamp_buy', 'symbol']).apply(
-            lambda g: g.sort_values('timestamp').assign(daily_pct=lambda df: df['price'].pct_change()).tail(1)).reset_index(drop=True)
+        latest = (
+            performance.groupby(["timestamp_buy", "symbol"])
+            .apply(
+                lambda g: g.sort_values("timestamp")
+                .assign(daily_pct=lambda df: df["price"].pct_change())
+                .tail(1)
+            )
+            .reset_index(drop=True)
+        )
         return latest
 
     def performance(self, indexes=None, anchor=None):
         indexes = indexes or default_indexes
         # print(indexes)
 
-        history = pd.concat([yf.stock(symbol, range='1y', interval='1d').dropna()
-                             for symbol in self.lots['symbol'].unique()], sort=False)
+        history = pd.concat(
+            [
+                yf.stock(symbol, range="10y", interval="1d").dropna()
+                for symbol in self.lots["symbol"].unique()
+            ],
+            sort=False,
+        )
         if anchor is not None:
-            history = history[history['timestamp'] <= anchor]
+            history = history[history["timestamp"] <= anchor]
 
         import datetime
+
         current_tz = datetime.datetime.now().astimezone().tzinfo
         dfh = history
-        dfo = self.lots.assign(timestamp=lambda df: df['timestamp'].dt.tz_localize(current_tz))
+        dfo = self.lots.assign(
+            timestamp=lambda df: df["timestamp"].dt.tz_localize(current_tz)
+        )
 
-        performance = dfo.merge(dfh[['symbol', 'timestamp', 'adjclose']].rename({'adjclose': 'price'}, axis=1),
-                                on='symbol', suffixes=('_buy', '')) \
-            .assign(value=lambda df: df['price']*df['quantity']) \
-            .assign(
-                gain=lambda df: df['value']-df['cost'],
-                gain_pct=lambda df: df['value']/df['cost']-1
+        performance = (
+            dfo.merge(
+                dfh[["symbol", "timestamp", "adjclose"]].rename(
+                    {"adjclose": "price"}, axis=1
+                ),
+                on="symbol",
+                suffixes=("_buy", ""),
             )
-        performance = performance[performance['timestamp_buy']<=performance['timestamp']]
+            .assign(value=lambda df: df["price"] * df["quantity"])
+            .assign(
+                gain=lambda df: df["value"] - df["cost"],
+                gain_pct=lambda df: df["value"] / df["cost"] - 1,
+            )
+        )
+        performance = performance[
+            performance["timestamp_buy"] <= performance["timestamp"]
+        ]
 
         return performance
-
-
-
-
-
